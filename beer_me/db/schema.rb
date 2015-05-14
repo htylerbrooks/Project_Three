b6eddb8b701c17941bc1154d4b6b309b5d580053
htylerbrooks <htb5qh@gmail.com>
2015-05-14 19:33:53 -0400

    clearning up staging area part deux

--- beer_me/db/schema.rb
+++ beer_me/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512193303) do
+ActiveRecord::Schema.define(version: 20150513234930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(version: 20150512193303) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
+
+  create_table "researches", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
